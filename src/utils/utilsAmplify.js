fc74ca73e7df929e8fe52cf1f4fc3d351d56a5e5
DylanGIV <dylangiv123@gmail.com>
2023-02-27 13:35:31 -0800

    converting more datastore to graphql api

--- src/utils/utilsAmplify.js
+++ src/utils/utilsAmplify.js
@@ -1,11 +1,8 @@
 import { API, Auth, graphqlOperation, Predicates, Storage } from 'aws-amplify';
 import {
   Company,
-  CompanyItemsImport,
-  Country,
   Item,
   Rfq,
-  RfqItems,
   ShippingAddress,
   UserDetails,
   UserDetailsShippingAddress,
@@ -14,7 +11,6 @@ import { DataStore } from 'aws-amplify';
 import countryList from '../countryList.json';
 import { v4 as uuidv4 } from 'uuid';
 import React from 'react';
-import { useAuthenticator } from '@aws-amplify/ui-react';
 import * as queries from '../graphql/queries.ts';
 import * as mutations from '../graphql/mutations.ts';
 
@@ -90,7 +86,13 @@ export const GetPartsByCompanySubscribe = async (
   const parts = response.data.itemsByCompanyID.items;
 
   // console.log(parts);
-  setData({ company: company, parts: parts, nextToken: null });
+  setData({
+    company: company,
+    parts: parts,
+    nextToken: response.data.itemsByCompanyID.nextToken
+      ? response.data.itemsByCompanyID.nextToken
+      : null,
+  });
   return response.data.itemsByCompanyID.nextToken;
 };
 export const GetRFQByCompany = async (
@@ -101,81 +103,42 @@ export const GetRFQByCompany = async (
   filter,
   userDetails
 ) => {
-  let count = 0;
   let rfqs = null;
   if (filter === 'sent') {
-    count = await DataStore.query(Rfq, (p) =>
-      p.sendingCompanyID.eq(company.id)
+    const response = await API.graphql(
+      graphqlOperation(queries.listRfqs, {
+        filter: {
+          sendingCompanyID: { eq: company.id },
+        },
+      })
     );
-    rfqs = await DataStore.query(
-      Rfq,
-      (p) => p.sendingCompanyID.eq(company.id),
-      {
-        page: page - 1,
-        limit: itemsPerPage,
-      }
-    );
+    rfqs = response.data.listRfqs.items;
   } else if (filter === 'received') {
-    count = await DataStore.query(Rfq, (p) =>
-      p.receivingCompanyID.eq(company.id)
+    const response = await API.graphql(
+      graphqlOperation(queries.listRfqs, {
+        filter: {
+          receivingCompanyID: { eq: company.id },
+        },
+      })
     );
-    rfqs = await DataStore.query(
-      Rfq,
-      (p) => p.receivingCompanyID.eq(company.id),
-      {
-        page: page - 1,
-        limit: itemsPerPage,
-      }
-    );
+    rfqs = response.data.listRfqs.items;
   } else {
-    count = await DataStore.query(Rfq, (p) =>
-      p.or((p) => [
-        p.receivingCompanyID.eq(company.id),
-        p.sendingCompanyID.eq(company.id),
-      ])
+    const response = await API.graphql(
+      graphqlOperation(queries.listRfqs, {
+        filter: {
+          or: [
+            { receivingCompanyID: { eq: company.id } },
+            { sendingCompanyID: { eq: company.id } },
+          ],
+        },
+      })
     );
-    rfqs = await DataStore.query(
-      Rfq,
-      (p) =>
-        p.or((p) => [
-          p.receivingCompanyID.eq(company.id),
-          p.sendingCompanyID.eq(company.id),
-        ]),
-      {
-        page: page - 1,
-        limit: itemsPerPage,
-      }
-    );
+    rfqs = response.data.listRfqs.items;
   }
   setRFQs({
     items: rfqs,
-    count: Math.ceil(count.length / itemsPerPage),
+    count: 1,
   });
-};
-export const GetPartsByCompanyAndSearch = async (
-  companyID,
-  search,
-  searchUpper,
-  searchLower
-) => {
-  const parts = await DataStore.query(
-    Item,
-    (p) =>
-      p.companyID.eq(companyID) &&
-      (p.altPartNumber.contains(search) ||
-        p.nsn.contains(search) ||
-        p.partNumber.contains(search) ||
-        p.description.contains(search) ||
-        p.altPartNumber.contains(searchLower) ||
-        p.nsn.contains(searchLower) ||
-        p.partNumber.contains(searchLower) ||
-        p.description.contains(searchLower) ||
-        p.altPartNumber.contains(searchUpper) ||
-        p.nsn.contains(searchUpper) ||
-        p.partNumber.contains(searchUpper) ||
-        p.description.contains(searchUpper))
-  );
-  return parts;
 };
 export const GetCompanyByID = async (companyID) => {
   const company = await API.graphql(
@@ -187,57 +150,78 @@ export const GetAllCompanies = async () => {
   const companies = await API.graphql(graphqlOperation(queries.listCompanies));
   return companies.data.listCompanies.items;
 };
-
-export const CreateUserDetails = async (
-  // companyID,
-  isOwner,
-  user,
-  userDetails
-) => {
-  console.log(user.username);
-  const existingUser = await DataStore.query(UserDetails, user.username);
-  console.log(existingUser);
+export const CreateUserDetails = async (isOwner, user, userDetails) => {
+  const existingUserResponse = await API.graphql(
+    graphqlOperation(queries.getUserDetails, { id: user.username })
+  );
+  const existingUser = existingUserResponse.data.getUserDetails;
   if (existingUser) {
-    console.log('enter');
     return existingUser;
   }
-  const response = await DataStore.save(
-    new UserDetails({
-      // companyID: companyID,
-      isCompanyOwner: isOwner,
-      userID: user.username,
-      firstName: userDetails.firstName,
-      lastName: userDetails.lastName,
-      contactEmail: userDetails.contactEmail,
-      contactPhone: userDetails.contactPhone,
+
+  const response = await API.graphql(
+    graphqlOperation(mutations.createUserDetails, {
+      input: {
+        id: user.username,
+        isCompanyOwner: isOwner,
+        firstName: userDetails.firstName,
+        lastName: userDetails.lastName,
+        contactEmail: userDetails.contactEmail,
+        contactPhone: userDetails.contactPhone,
+      },
     })
   );
-  console.log(response);
-  return response;
+
+  console.log(response.data.createUserDetails);
+  return response.data.createUserDetails;
 };
 
 export const AddUserShippingAddress = async (shippingAddress, user) => {
-  const userDetails = await DataStore.query(UserDetails, user.username);
+  const userDetailsResponse = await API.graphql(
+    graphqlOperation(queries.getUserDetails, { id: user.username })
+  );
+  const userDetails = userDetailsResponse.data.getUserDetails;
+  if (!userDetails) {
+    alert(
+      'User details not found, could not add shipping address. If this issue persists, please contact support.'
+    );
+    return;
+  }
   try {
-    const newShippingAddress = await DataStore.save(
-      new ShippingAddress({
-        addressLine1: shippingAddress.addressLine1,
-        addressLine2: shippingAddress.addressLine2,
-        city: shippingAddress.city,
-        postalCode: shippingAddress.postalCode,
-        regi: shippingAddress.regi,
-        streetNumber: shippingAddress.streetNumber,
-        unitNumber: shippingAddress.unitNumber,
-        countryID: shippingAddress.countryID,
+    const newShippingAddressResponse = await API.graphql(
+      graphqlOperation(mutations.createShippingAddress, {
+        input: {
+          addressLine1: shippingAddress.addressLine1,
+          addressLine2: shippingAddress.addressLine2,
+          city: shippingAddress.city,
+          postalCode: shippingAddress.postalCode,
+          state: shippingAddress.state,
+          streetNumber: shippingAddress.streetNumber,
+          unitNumber: shippingAddress.unitNumber,
+          countryID: shippingAddress.countryID,
+        },
       })
     );
+    const newShippingAddress =
+      newShippingAddressResponse.data.createShippingAddress;
+    if (!newShippingAddress) {
+      alert(
+        'Could not add shipping address. If this issue persists, please contact support.'
+      );
+      return;
+    }
     try {
-      await DataStore.save(
-        new UserDetailsShippingAddress({
-          shippingAddress: newShippingAddress,
-          userDetails: userDetails,
+      const response = await API.graphql(
+        graphqlOperation(mutations.createUserDetailsShippingAddress, {
+          input: {
+            shippingAddressID: newShippingAddress.id,
+            userDetailsID: userDetails.id,
+            shippingAddress: newShippingAddress,
+            userDetails: userDetails,
+          },
         })
       );
+      console.log(response);
     } catch (error) {
       console.log(error);
     }
@@ -248,54 +232,46 @@ export const AddUserShippingAddress = async (shippingAddress, user) => {
 
 export const CreateRFQ = async (rfqDetails) => {
   try {
-    const rfqResponse = await DataStore.save(
-      new Rfq({
-        rfqNumber: rfqDetails.rfqNumber,
-        addressLine1: rfqDetails.addressLine1,
-        addressLine2: rfqDetails.addressLine2,
-        city: rfqDetails.city,
-        country: rfqDetails.country,
-        state: rfqDetails.state,
-        zip: rfqDetails.zip,
-        phone: rfqDetails.phone,
-        email: rfqDetails.email,
-        uom: rfqDetails.uom,
-        discount: rfqDetails.discount,
-        companyName: rfqDetails.companyName,
-        contact: rfqDetails.contact,
-        custRefNum: rfqDetails.custRefNum,
-        dateSent: rfqDetails.dateSent,
-        dueDate: rfqDetails.dueDate,
-        emailComments: rfqDetails.emailComments,
-        imageUrls: rfqDetails.imageUrls,
-        internalComments: rfqDetails.internalComments,
-        leadTime: rfqDetails.leadTime,
-        lineTotal: rfqDetails.lineTotal,
-        paymentTerms: rfqDetails.paymentTerms,
-        quantityQuoted: rfqDetails.quantityQuoted,
-        quantityRequested: rfqDetails.quantityRequested,
-        salesTax: rfqDetails.salesTax,
-        shippingMethod: rfqDetails.shippingMethod,
-        shippingTerms: rfqDetails.shippingTerms,
-        subtotal: rfqDetails.subtotal,
-        total: rfqDetails.total,
-        receivingCompanyID: rfqDetails.receivingCompanyID,
-        sendingCompanyID: rfqDetails.sendingCompanyID,
-        urgency: rfqDetails.urgency,
-        itemIDs: rfqDetails.itemIDs,
+    const rfqResponse = await API.graphql(
+      graphqlOperation(mutations.createRfq, {
+        input: {
+          rfqNumber: rfqDetails.rfqNumber,
+          addressLine1: rfqDetails.addressLine1,
+          addressLine2: rfqDetails.addressLine2,
+          city: rfqDetails.city,
+          country: rfqDetails.country,
+          state: rfqDetails.state,
+          zip: rfqDetails.zip,
+          phone: rfqDetails.phone,
+          email: rfqDetails.email,
+          uom: rfqDetails.uom,
+          discount: rfqDetails.discount,
+          companyName: rfqDetails.companyName,
+          contact: rfqDetails.contact,
+          custRefNum: rfqDetails.custRefNum,
+          dateSent: rfqDetails.dateSent,
+          dueDate: rfqDetails.dueDate,
+          emailComments: rfqDetails.emailComments,
+          imageUrls: rfqDetails.imageUrls,
+          internalComments: rfqDetails.internalComments,
+          leadTime: rfqDetails.leadTime,
+          lineTotal: rfqDetails.lineTotal,
+          paymentTerms: rfqDetails.paymentTerms,
+          quantityQuoted: rfqDetails.quantityQuoted,
+          quantityRequested: rfqDetails.quantityRequested,
+          salesTax: rfqDetails.salesTax,
+          shippingMethod: rfqDetails.shippingMethod,
+          shippingTerms: rfqDetails.shippingTerms,
+          subtotal: rfqDetails.subtotal,
+          total: rfqDetails.total,
+          receivingCompanyID: rfqDetails.receivingCompanyID,
+          sendingCompanyID: rfqDetails.sendingCompanyID,
+          urgency: rfqDetails.urgency,
+          itemIDs: rfqDetails.itemIDs,
+        },
       })
     );
     console.log(rfqResponse);
-    // const rfq = await DataStore.query(Rfq, rfqResponse.id);
-    // for (let i = 0; i < items.length; i++) {
-    //   const response = await DataStore.save(
-    //     new RfqItems({
-    //       item: items[i],
-    //       rfq: rfq,
-    //     })
-    //   );
-    //   console.log(response);
-    // }
     alert('Successfully sent RFQ to ' + rfqDetails.companyName);
   } catch (error) {
     console.log(error);
@@ -312,56 +288,70 @@ export const AddCompany = async (
   profilePictureUrl,
   country
 ) => {
-  const response = await DataStore.save(
-    new Company({
-      companyName: companyName,
-      companyDescription: companyDescription,
-      contactEmail: email,
-      phone: phone,
-      fax: fax,
-      profilePictureUrl: profilePictureUrl,
-      countryID: country.code,
+  const response = await API.graphql(
+    graphqlOperation(mutations.createCompany, {
+      input: {
+        companyName: companyName,
+        companyDescription: companyDescription,
+        contactEmail: email,
+        phone: phone,
+        fax: fax,
+        profilePictureUrl: profilePictureUrl,
+        countryID: country.code,
+      },
     })
   );
-  return response;
+  return response.data.createCompany;
 };
 export const AddUserToCompany = async (userDetails, companyID) => {
-  const userToAdd = await DataStore.query(UserDetails, userDetails.userID);
-  const original = await DataStore.query(Company, companyID);
-  await DataStore.save(
-    UserDetails.copyOf(userToAdd, (user) => {
-      user.companyID = companyID;
+  const userToAddResponse = await API.graphql(
+    graphqlOperation(queries.getUserDetails, { id: userDetails.userID })
+  );
+  const userToAdd = userToAddResponse.data.getUserDetails;
+  const originalCompanyResponse = await API.graphql(
+    graphqlOperation(queries.getCompany, { id: companyID })
+  );
+  const originalCompany = originalCompanyResponse.data.getCompany;
+
+  await API.graphql(
+    graphqlOperation(mutations.updateUserDetails, {
+      input: { id: userToAdd.id, companyID: companyID },
     })
   );
-  const response = await DataStore.save(
-    Company.copyOf(original, (newCompany) => {
-      newCompany.CompanyMembers.push(userToAdd);
+  const response = await API.graphql(
+    graphqlOperation(mutations.updateCompany, {
+      input: {
+        id: originalCompany.id,
+        CompanyMembers: [...originalCompany.CompanyMembers, userToAdd],
+      },
     })
   );
   return response;
 };
 export const AddOwnerToCompany = async (userDetails, companyID) => {
-  const userToAdd = await DataStore.query(UserDetails, userDetails.userID);
-  const original = await DataStore.query(Company, companyID);
-  await DataStore.save(
-    UserDetails.copyOf(userToAdd, (user) => {
-      user.companyID = companyID;
+  const userToAddResponse = await API.graphql(
+    graphqlOperation(queries.getUserDetails, { id: userDetails.userID })
+  );
+  const userToAdd = userToAddResponse.data.getUserDetails;
+  const originalCompanyResponse = await API.graphql(
+    graphqlOperation(queries.getCompany, { id: companyID })
+  );
+  const originalCompany = originalCompanyResponse.data.getCompany;
+
+  await API.graphql(
+    graphqlOperation(mutations.updateUserDetails, {
+      input: { id: userToAdd.id, companyID: companyID },
     })
   );
-  const response = await DataStore.save(
-    Company.copyOf(original, (newCompany) => {
-      newCompany.CompanyOwner = userToAdd;
+  const response = await API.graphql(
+    graphqlOperation(mutations.updateCompany, {
+      input: {
+        id: originalCompany.id,
+        CompanyOwner: userToAdd,
+      },
     })
   );
   return response;
-};
-
-export const DeleteAllPartsByCompany = async (companyID) => {
-  return await DataStore.delete(Item, (p) => p.companyID.eq(companyID));
-};
-
-export const DeleteAllParts = async () => {
-  return await DataStore.delete(Item, Predicates.ALL);
 };
 
 export const PopulateCountries = async () => {
@@ -394,10 +384,21 @@ export const GetCountries = async () => {
 
 export const GetUserShippingAddresses = async (userDetails) => {
   try {
-    const userShippingAddresses = await DataStore.query(ShippingAddress, (p) =>
-      p.UserShippingAddresses.userDetails.userID.eq(userDetails.userID)
+    const userShippingAddressesResponse = await API.graphql(
+      graphqlOperation(queries.listUserDetailsShippingAddresses, {
+        filter: {
+          userDetailsUserID: {
+            eq: userDetails.userID,
+          },
+        },
+      })
     );
-    return userShippingAddresses;
+    console.log(
+      userShippingAddressesResponse.data.listUserDetailsShippingAddresses.items
+    );
+    return userShippingAddressesResponse.data.listUserDetailsShippingAddresses.items.map(
+      (item) => item.shippingAddress
+    );
   } catch (error) {
     console.log(error);
   }
